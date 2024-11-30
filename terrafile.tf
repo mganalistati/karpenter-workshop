c4f4ab6680c2aa3da33c9c0fb07ebe320dd2890d
Murillo Gomes <mganalistati@gmail.com>
2024-11-30 14:59:14 -0300

    chore: renamed provider to providers and adding new input on terrafile to cluster module

--- terrafile.tf
+++ terrafile.tf
@@ -13,4 +13,28 @@ module "eks_cluster" {
   common_tags       = local.common_tags
   ctp_private_sn_1a = module.eks_network.ctp_private_sn_1a
   ctp_private_sn_2b = module.eks_network.ctp_private_sn_2b
+  ctp_private_sn_3c = module.eks_network.ctp_private_sn_3c
+}
+
+module "eks_node_group" {
+  source = "./modules/node_group"
+
+  cluster_name = module.eks_cluster.cluster_name
+  project_name = var.project_name
+
+  # validar como fazer - Error: Unsupported attribute
+  # subnet_ids = [
+  #   module.eks_network.pod_private_sn_1a,
+  #   module.eks_network.pod_private_sn_2b,
+  #   module.eks_network.pod_private_sn_3c
+  # ]
+
+  pod_private_sn_1a = module.eks_network.pod_private_sn_1a
+  pod_private_sn_2b = module.eks_network.pod_private_sn_2b
+  pod_private_sn_3c = module.eks_network.pod_private_sn_3c
+
+  capacity_type = "SPOT"
+  instance_type = ["m5.large"]
+
+  common_tags = local.common_tags
 }
